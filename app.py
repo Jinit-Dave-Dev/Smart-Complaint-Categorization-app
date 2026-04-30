8101b8aae1c7eb374ab7364e7cb87f7d530c8f1d
Jinit-Dave-Dev <davejinit04@gmail.com>
2026-04-30 23:49:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ html, body, [data-testid="stAppViewContainer"] {
 
 /* Background image (government style) */
 [data-testid="stAppViewContainer"] {
-    background: url("https://images.unsplash.com/photo-1581093588401-22a2c0c3b6a2") no-repeat center center fixed;
+    background: url("https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b") no-repeat center center fixed;
     background-size: cover;
 }
 
